81225556e24edba5620ee487815a4adb5ad4976c
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-06-11 12:55:16 +0700

    Update: Pages - componets - services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PostComponent,
+    ProjectComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
